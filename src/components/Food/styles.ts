2a8a39cce675ab07a0e3c4d3453394fd6c47cfa8
Will Apolinario <willieapolinario@gmail.com>
2021-09-10 17:06:55 -0300

    challenge done

--- src/components/Food/styles.ts
+++ src/components/Food/styles.ts
@@ -1,6 +1,10 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerFoodCardStyledProps {
+  available: boolean
+}
+
+export const Container = styled.div<ContainerFoodCardStyledProps>`
   background: #f0f0f5;
   border-radius: 8px;
 
--- src/components/Input/styles.ts
+++ src/components/Input/styles.ts
@@ -1,6 +1,11 @@
 import styled, { css } from 'styled-components';
 
-export const Container = styled.div`
+interface ContainerInputStyledProps {
+  isFocused: boolean,
+  isFilled: boolean
+}
+
+export const Container = styled.div<ContainerInputStyledProps>`
   display: flex;
   align-items: center;
 
